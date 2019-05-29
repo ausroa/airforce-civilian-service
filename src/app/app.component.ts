0e1064a1a89028db9521e5acaf8b011f96f77359
Austin Roa <ausroa@gmail.com>
2019-05-29 15:12:23 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'afcs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'airforce-civilian-service';
+}
