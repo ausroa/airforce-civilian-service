44c3dc5281e4b16ce3e705fd9eb69e77838d209d
Austin Roa <ausroa@gmail.com>
2019-06-01 16:12:43 -0400

    created slider

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'afcs-slider',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit {
+  slides: any[] = [
+    {name: '', url: ''},
+    {name: '', url: ''},
+    {name: '', url: ''},
+    {name: '', url: ''},
+    {name: '', url: ''}
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  @HostListener('click', ['$event'])
+  click(e) {
+    console.log('mouse was clicked' , e.srcElement.id);
+  }
 }
