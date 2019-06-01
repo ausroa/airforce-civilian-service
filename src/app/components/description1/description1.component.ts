842e97759ce27c6d012bb1e11aedf5363b6dafa7
Austin Roa <ausroa@gmail.com>
2019-06-01 03:26:55 -0400

    finished description1

--- src/app/components/description1/description1.component.ts
+++ src/app/components/description1/description1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'afcs-description1',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./description1.component.scss']
 })
 export class Description1Component implements OnInit {
+  startType = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  @HostListener('window:scroll', ['$event'])
+  startTyping() {
+    const componentLocation = 700;
+    if(window.pageYOffset === componentLocation) {
+      this.startType = true;
+    }
+  }
 }
