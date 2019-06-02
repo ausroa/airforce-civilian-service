3e77036dc6c38641fd9b45d0cae48874f0923ee4
Austin Roa <ausroa@gmail.com>
2019-06-02 18:38:44 -0400

    tilebuttons2 start

--- src/app/components/tile-buttons2/tile-buttons2.component.ts
+++ src/app/components/tile-buttons2/tile-buttons2.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tile-buttons2.component.scss']
 })
 export class TileButtons2Component implements OnInit {
+  tileButtons: any[] = [
+    {title: 'News'},
+    {title: 'Events'}
+  ];
 
   constructor() { }
 
