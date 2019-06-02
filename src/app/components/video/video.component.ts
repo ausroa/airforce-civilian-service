9b0640ffc240fe42f8e7ba20222e78dcc3d6faaa
Austin Roa <ausroa@gmail.com>
2019-06-02 16:53:05 -0400

    video created and layers added

--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'afcs-video',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video.component.scss']
 })
 export class VideoComponent implements OnInit {
+  startVideo = false;
 
   constructor() { }
 
   ngOnInit() {
+    console.log('Video Location: ', window.pageYOffset);
   }
 
+  @HostListener('window:scroll', ['$event'])
+  showComponent() {
+    const componentLocation = 3400;
+    if(window.pageYOffset === componentLocation) {
+      this.startVideo = true;
+    }
+  }
 }
